ac3b1feb6774ef7daf7cf666ffc44e7f54548936
Eyob Mekonnen <mekoneneyob@5gmail.com>
2024-08-13 11:18:21 -0700

    change dist file

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,29 +1,31 @@
-import icons from 'url:../../img/icons.svg'
+import icons from 'url:../../img/icons.svg';
 import view from './view.js';
-import {Fraction} from 'fractional'
-class RecipeView extends view{
-    _parentElement=document.querySelector('.recipe');
-    _defaultErrorMessage="no recipe found ";
-    addLoadHandler(handler){
-      ['hashchange','load'].forEach(event=>window.addEventListener(event,handler));
-    }
-    addServingHandler(handler){
-      this._parentElement.addEventListener('click',function(e){
-        const btn=e.target.closest('.btn--update-servings');
-        if(!btn) return;
-        const {update}=btn.dataset;
-        if(+update > 0)  handler(+update);
-      })
-    }
-    addBookMarkHandler(handler){
-      this._parentElement.addEventListener('click',function(e){
-        const btn=e.target.closest('.btn--bookmark');
-        if(!btn) return;
-        handler();
-      })
-    }
-    _generateMarkup(){ 
-return `
+import { Fraction } from 'fractional';
+class RecipeView extends view {
+  _parentElement = document.querySelector('.recipe');
+  _defaultErrorMessage = 'no recipe found ';
+  addLoadHandler(handler) {
+    ['hashchange', 'load'].forEach(event =>
+      window.addEventListener(event, handler)
+    );
+  }
+  addServingHandler(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--update-servings');
+      if (!btn) return;
+      const { update } = btn.dataset;
+      if (+update > 0) handler(+update);
+    });
+  }
+  addBookMarkHandler(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--bookmark');
+      if (!btn) return;
+      handler();
+    });
+  }
+  _generateMarkup() {
+    return `
     <figure class="recipe__fig">
       <img src="${this._data.image}" alt="Tomato" class="recipe__img" />
              <h1 class="recipe__title">
@@ -36,23 +38,31 @@ return `
    <svg class="recipe__info-icon">
      <use href="src/img/icons.svg#icon-clock"></use>
    </svg>
-   <span class="recipe__info-data recipe__info-data--minutes">${this._data.cookingTime}</span>
+   <span class="recipe__info-data recipe__info-data--minutes">${
+     this._data.cookingTime
+   }</span>
    <span class="recipe__info-text">minutes</span>
  </div>
  <div class="recipe__info">
    <svg class="recipe__info-icon">
      <use href="${icons}#icon-users"></use>
    </svg>
-   <span class="recipe__info-data recipe__info-data--people">${this._data.servings}</span>
+   <span class="recipe__info-data recipe__info-data--people">${
+     this._data.servings
+   }</span>
    <span class="recipe__info-text">servings</span>
 
    <div class="recipe__info-buttons">
-     <button class="btn--tiny btn--update-servings" data-update="${this._data.servings - 1}">
+     <button class="btn--tiny btn--update-servings" data-update="${
+       this._data.servings - 1
+     }">
        <svg>
          <use href="${icons}#icon-minus-circle"></use>
        </svg>
      </button>
-     <button class="btn--tiny btn--update-servings" data-update="${this._data.servings + 1}">
+     <button class="btn--tiny btn--update-servings" data-update="${
+       this._data.servings + 1
+     }">
        <svg>
          <use href="${icons}#icon-plus-circle"></use>
        </svg>
@@ -67,7 +77,9 @@ return `
  </div>
  <button class="btn--round btn--bookmark">
    <svg class="">
-     <use href="${icons}#icon-bookmark${this._data.bookmarked ? '-fill' : ''}"></use>
+     <use href="${icons}#icon-bookmark${
+      this._data.bookmarked ? '-fill' : ''
+    }"></use>
    </svg>
  </button>
 </div>
@@ -75,20 +87,24 @@ return `
 <div class="recipe__ingredients">
  <h2 class="heading--2">Recipe ingredients</h2>
  <ul class="recipe__ingredient-list">
- ${this._data.ingredients.map(ing=>{
-   return `
+ ${this._data.ingredients
+   .map(ing => {
+     return `
    <li class="recipe__ingredient">
      <svg class="recipe__icon">
        <use href="${icons}#icon-check"></use>
      </svg>
-     <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+     <div class="recipe__quantity">${
+       ing.quantity ? ing.quantity.toString() : ''
+     }</div>
      <div class="recipe__description">
        <span class="recipe__unit">${ing.unit}</span>
        ${ing.description}
      </div>
    </li>
    `;
- }).join('')}
+   })
+   .join('')}
  </ul>
 </div>
 
@@ -96,7 +112,9 @@ return `
  <h2 class="heading--2">How to cook it</h2>
  <p class="recipe__directions-text">
    This recipe was carefully designed and tested by
-   <span class="recipe__publisher">${this._data.publisher}</span>. Please check out
+   <span class="recipe__publisher">${
+     this._data.publisher
+   }</span>. Please check out
    directions at their website.
  </p>
  <a
@@ -111,7 +129,6 @@ return `
  </a>
 </div>
 `;
-    }
+  }
 }
-export default new RecipeView()
-
+export default new RecipeView();
--- src/js/views/searchResultView.js
+++ src/js/views/searchResultView.js
@@ -1,23 +1,26 @@
 import view from './view.js';
-import icons from 'url:../../img/icons.svg'
-class resultView extends view{
-    _parentElement=document.querySelector('.results');
-    _defaultErrorMessage="no search result found";
-    _generateMarkup(){
-      console.log(this._data)
-        return `
-        ${this._data.map((result)=>{
-          const id=window.location.hash.slice(1);
-           return `
+import icons from 'url:../../img/icons.svg';
+class resultView extends view {
+  _parentElement = document.querySelector('.results');
+  _defaultErrorMessage = 'no search result found';
+  _generateMarkup() {
+    return `
+        ${this._data.map(result => {
+          const id = window.location.hash.slice(1);
+          return `
           <li class="preview">
-            <a class="preview__link ${result.id === id ? "preview__link--active" : ''} " href="#${result.id}">
+            <a class="preview__link ${
+              result.id === id ? 'preview__link--active' : ''
+            } " href="#${result.id}">
               <figure class="preview__fig">
                 <img src="${result.image}" alt="Test" />
               </figure>
               <div class="preview__data">
                 <h4 class="preview__title">${result.title}</h4>
                 <p class="preview__publisher">${result.publisher}</p>
-                <div class="preview__user-generated ${result.key ? '' : 'hidden' }">
+                <div class="preview__user-generated ${
+                  result.key ? '' : 'hidden'
+                }">
                   <svg>
                     <use href="${icons}#icon-user"></use>
                   </svg>
@@ -25,8 +28,8 @@ class resultView extends view{
               </div>
             </a>
           </li>
-          `}
-          )}`
-       }
+          `;
+        })}`;
+  }
 }
 export default new resultView();
